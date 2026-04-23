cbcfad5390b179937db19810ad7772130ab6c6e1
Gonza-BR <gonzibrandan2@gmail.com>
2026-04-23 02:24:36 -0300

    primer cambio

--- holamundo.py
+++ holamundo.py
@@ -1,3 +1,20 @@
-print("hola")
-print("modificando")
-print("mejor este cambio")
+#nuevo menu
+
+print("menu")
+print("1. opcion 1")
+print("2. opcion 2")
+print("3. opcion 3") 
+print("4. opcion 4") 
+opcion = input("ingrese una opcion: ")
+
+if opcion == "1": 
+    print("opcion 1 seleccionada")
+elif opcion == "2": 
+    print("opcion 2 seleccionada")
+elif opcion == "3": 
+    print("opcion 3 seleccionada")
+elif opcion == "4": 
+    print("opcion 4 seleccionada")
+else: 
+    print("opcion no valida")
+    
